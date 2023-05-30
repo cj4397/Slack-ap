307b2bf1a046f4b11fd4dced36f1ba1a4e70a1dd
cj4397 <calbo12244397@gmail.com>
2023-05-30 19:38:20 +0800

    get user progress

--- src/app/dashboard/(pages)/DM/page.tsx
+++ src/app/dashboard/(pages)/DM/page.tsx
@@ -1,6 +1,29 @@
+'use client';
+import { datas } from "@/app/auth";
 
 
 export default function DM() {
+    const { user_data } = datas()
+    const { token, client, expiry, uid } = user_data
+
+    const sendMessage = async () => {
+
+        const send = await fetch('http://206.189.91.54/api/v1/users', {
+            method: 'GET',
+            headers: {
+                'access-token': token,
+                'client': client,
+                'expiry': expiry,
+                'uid': uid,
+            }
+        })
+
+        const body = await send.json()
+        console.log(body)
+
+
+    }
+
     return (
         <>DM page</>
     )
