380b9f2ac99bcd133ee21d05e2fa75d8f6e3606c
cj4397 <calbo12244397@gmail.com>
2023-05-31 15:29:53 +0800

    try

--- src/app/dashboard/(pages)/search/page.tsx
+++ src/app/dashboard/(pages)/search/page.tsx
@@ -1,20 +1,15 @@
 'use client';
 import { datas } from "@/app/auth";
+import { useEffect, useState } from "react";
 
 
 
 export default function Seach() {
     const { user_data } = datas()
     const { token, client, expiry, uid } = user_data
-    console.log(token, client, expiry, uid)
 
 
-
-
-
-
-    const showUsers = async () => {
-
+    const userListData = async () => {
         const getUsersResponse = await fetch('http://206.189.91.54/api/v1/users', {
             method: 'GET',
             headers: {
@@ -24,16 +19,46 @@ export default function Seach() {
                 'uid': uid,
             }
         })
-
         const body = await getUsersResponse.json()
-        console.log(body)
-
-
+        setUserList(body)
+        console.log(userList)
+        return body
     }
+    const [userList, setUserList] = useState()
+
+
+
+    useEffect(() => {
+        // userListData()
+        console.log(userList)
+    })
+
+
+    // const showUsers = async () => {
+
+    //     const getUsersResponse = await fetch('http://206.189.91.54/api/v1/users', {
+    //         method: 'GET',
+    //         headers: {
+    //             'access-token': token,
+    //             'client': client,
+    //             'expiry': expiry,
+    //             'uid': uid,
+    //         }
+    //     })
+
+    //     const body = await getUsersResponse.json()
+    //     console.log(body)
+
+
+    // }
+
+
     return (
         <>
             Seach page
             <button onClick={() => showUsers()}>users</button>
+
+
         </>
     )
 }
