2ac801234244be1886f0e07ecf8f6a1d1dda29ad
cj4397 <calbo12244397@gmail.com>
2023-06-09 18:39:52 +0800

    remove item and private route

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,26 +17,26 @@ export default function DashboardLayout(props: { children: React.ReactNode }) {
   };
 
   return (
-    <PrivateRoute>
-      <section>
-        <Grid2 container spacing={2}>
-          <Grid2 item xs={2}>
-            <Stack direction="column" spacing={2}>
-              <button onClick={handleLogout}>Logout</button>
-              <div>
-                <h2>Channels</h2>
-                <button>Add Channel</button>
-              </div>
-              <div>
-                <h1>Direct Messages</h1>
-              </div>
-            </Stack>
-          </Grid2>
-          <Grid2 item xs={12} md={9}>
-            {props.children}
-          </Grid2>
+
+    <section>
+      <Grid2 container spacing={2}>
+        <Grid2 xs={2}>
+          <Stack direction="column" spacing={2}>
+            <button onClick={handleLogout}>Logout</button>
+            <div>
+              <h2>Channels</h2>
+              <button>Add Channel</button>
+            </div>
+            <div>
+              <h1>Direct Messages</h1>
+            </div>
+          </Stack>
         </Grid2>
-      </section>
-    </PrivateRoute>
+        <Grid2 xs={12} md={9}>
+          {props.children}
+        </Grid2>
+      </Grid2>
+    </section>
+
   );
 }
